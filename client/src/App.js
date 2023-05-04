86c4bc37e0c734455402c576436bd71565fcdc1b
Ester Cortes <002escor@gmail.com>
2023-05-04 15:15:13 +0200

    feat: added post request

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,24 @@
 import "./App.css";
 import { useEffect, useState } from "react";
-import Messages  from "./components/Messages"
+import Messages from "./components/Messages";
 
 function App() {
-	const [messagesData, setMessagesData] = useState([])
+	const [messagesData, setMessagesData] = useState([]);
 
-	
-	useEffect( () => {
+	useEffect(() => {
 		const getData = async () => {
 			const res = await fetch("/messages"); // replace with your Express server's URL
 			const data = await res.json();
 			setMessagesData(data);
-		  };
+		};
 
 		getData();
-		
+		console.log(messagesData);
 	}, []);
 	return (
 		<div className="App-header">
 			<h1>wdawad</h1>
-			<Messages messagesData={messagesData}/>
+			<Messages messagesData={messagesData} />
 		</div>
 	);
 }
--- server/router/router.js
+++ server/router/router.js
@@ -1,16 +1,25 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const MessageTable = require("../model.js")
+const MessageTable = require("../model.js");
 
-console.log(MessageTable)
+console.log(MessageTable);
 
-router.get('/messages', async (req, res) => {
-  // const data = {
-  //   username:"happyTime",
-  //   message: 'Hello from Express!'
-  // };
-  const list = await MessageTable.findAll();
+router.get("/messages", async (req, res) => {
+	// const data = {
+	//   username:"happyTime",
+	//   message: 'Hello from Express!'
+	// };
+	const list = await MessageTable.findAll();
 	res.json(list);
+});
+
+router.post("/message", async (req, res) => {
+	const { username, message } = req.body;
+	const newMessage = await MessageTable.create({
+		username,
+		message,
+	});
+	res.send(newMessage);
 });
 
 module.exports = router;
