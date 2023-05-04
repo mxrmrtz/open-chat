fe7f164d5942597764df65e1eee889ade0ee8552
Maximiliam Reimertz <max.reimertz@gmail.com>
2023-05-04 18:17:47 +0200

    testing the stash

--- client/src/App.js
+++ client/src/App.js
@@ -5,18 +5,35 @@ import ChatBox from "./components/ChatBox";
 function App() {
 	const [messagesData, setMessagesData] = useState([]);
 
-	useEffect(() => {
-		const getData = async () => {
+	const getData = async () => {
+		try {
 			const res = await fetch("/messages");
 			const data = await res.json();
 			setMessagesData(data);
-		};
+		} catch (error) {
+			console.error(error);
+		}
+	};
+
+	const handleDelete = async (id) => {
+		try {
+			const res = await fetch(`/messages/${id}`, {
+				method: "DELETE",
+				headers: { "Content-Type": "application/json" },
+			});
+			getData();
+		} catch (error) {
+			alert("heck");
+		}
+	};
+
+	useEffect(() => {
 		getData();
 	}, []);
 	return (
 		<div className="App-header">
 			<h1>Wacky Chaty</h1>
-			<ChatBox messagesData={messagesData} />
+			<ChatBox handleDelete={handleDelete} messagesData={messagesData} />
 		</div>
 	);
 }
---
+++ client/src/components/messages/Messages.js
@@ -0,0 +1,26 @@
+import styles from "./messages.module.css";
+import MoreButton from "./MoreButton";
+
+const Messages = ({ messagesData, handleDelete }) => {
+	return (
+		<>
+			<ul>
+				{messagesData.map((item) => {
+					return (
+						<li key={item.id}>
+							<div className={styles.username}>
+								<h3>{item.username}</h3>
+							</div>
+							<div className={styles.message}>
+								<p className={styles.message_text}>{item.message}</p>
+							</div>
+							<MoreButton id={item.id} handleDelete={handleDelete} className={styles.more} />
+						</li>
+					);
+				})}
+			</ul>
+		</>
+	);
+};
+
+export default Messages;
