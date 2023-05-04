611239090caab330533a737504fe84584b49ffb3
Ester Cortes <002escor@gmail.com>
2023-05-04 15:24:07 +0200

    feat: added put request

--- server/router/router.js
+++ server/router/router.js
@@ -1,16 +1,29 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const MessageTable = require("../model.js")
+const MessageTable = require("../model.js");
 
-console.log(MessageTable)
+console.log(MessageTable);
 
-router.get('/messages', async (req, res) => {
-  // const data = {
-  //   username:"happyTime",
-  //   message: 'Hello from Express!'
-  // };
-  const list = await MessageTable.findAll();
+router.get("/messages", async (req, res) => {
+	// const data = {
+	//   username:"happyTime",
+	//   message: 'Hello from Express!'
+	// };
+	const list = await MessageTable.findAll();
 	res.json(list);
+});
+
+router.put("/message", async (req, res) => {
+	const { message, id } = req.body;
+	const updatedMessage = await MessageTable.update(
+		{ message: message },
+		{
+			where: {
+				id: id,
+			},
+		}
+	);
+	res.send(updatedMessage);
 });
 
 module.exports = router;
