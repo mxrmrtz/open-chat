205c26536c9df18b583d4b7fe2dbce108dde1472
Maximiliam Reimertz <max.reimertz@gmail.com>
2023-05-05 16:47:08 +0200

    Added some styling to chatbox

--- client/src/components/ChatBox.js
+++ client/src/components/ChatBox.js
@@ -1,14 +1,14 @@
 import styles from "./chatbox.module.css";
 import ChatInput from "./ChatInput";
-import Messages from "./Messages";
+import Messages from "./messages/Messages.js";
 import Profiles from "./Profiles";
 
 const ChatBox = ({ messagesData, handleNewMessage }) => {
 	return (
 		<>
 			<div className={styles.container}>
-				<Profiles messagesData={messagesData} className={styles.testing} />
-				<div>
+				<Profiles messagesData={messagesData} className={styles.profiles_container} />
+				<div className={styles.chatbox_container}>
 					<Messages messagesData={messagesData} />
 					<ChatInput handleNewMessage={handleNewMessage} />
 				</div>
