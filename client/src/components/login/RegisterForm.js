84b14bad31db3be84950bca029e516e9842b4259
Maximiliam Reimertz <max.reimertz@gmail.com>
2023-05-06 16:41:31 +0200

    created the final coed for adding user ass

--- client/src/components/login/RegisterForm.js
+++ client/src/components/login/RegisterForm.js
@@ -1,18 +1,36 @@
-const RegisterForm = ({ setShowRegister }) => {
+const RegisterForm = ({ setShowRegister, setUser, user }) => {
+	const handleAccount = (e) => {
+		const Inputuser = { username: e.target[0].value, password: e.target[1].value };
+		setUser((prev) => ({ ...prev, ...Inputuser }));
+		e.preventDefault();
+	};
+
+	// const handleChange = (e) => {
+	// 	e.target.id.preventDefault();
+	// 	setUser((prev) => ({ ...prev, id: e.target.value }));
+	// };
+
 	return (
 		<>
-			<form>
-				<input type="text" placeholder="username" />
-				<input type="text" placeholder="password" />
+			<form onSubmit={handleAccount}>
+				<input type="text" id="username" placeholder="username" />
+				<input type="text" id="password" placeholder="password" />
 				<button type="submit">Register</button>
 			</form>
+			<button
+				onClick={() => {
+					setUser({ username: "cringe", password: "kewl" });
+				}}
+			>
+				Create a cringe account
+			</button>
 			<button
 				type="button"
 				onClick={() => {
 					setShowRegister(false);
 				}}
 			>
-				GO bakc to login
+				Already have an account?
 			</button>
 		</>
 	);
